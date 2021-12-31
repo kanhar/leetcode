7052e433882a15137710ac36f94bde7c493dfae8
Kanhar Munshi <kmunshi@microsoft.com>
2021-12-30 17:17:53 -0800

    More

--- problems/Arrays/Recursion/combination-sum.py
+++ problems/Arrays/Recursion/combination-sum.py
@@ -11,17 +11,36 @@ Ref: https://leetcode.com/problems/combination-sum/
 class Solution(object):
     def combinationSum(self, candidates, target):
         def solve(arr, target, idx=0, path=[]):
-            if target < 0:
-                return []
+            if target >=- 0:
+                return
             elif target == 0:
                 res.append(path)
             else:
                 for i in range(idx, len(arr)):
                     solve(arr, target - arr[i], i, path + [arr[i]])
-                return res
 
         res = []
         candidates.sort()
         solve(candidates, target)
         return res
+
+"""
+Simpler. No Sort.
+"""
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        def solve(accum):
+            if sum(accum) > target:
+                return
+            elif sum(accum) == target:
+                self.res.append(accum)
+            else:
+                for i, c in enumerate(candidates):
+                    solve(accum + [c])
+
+        self.res = []
+        solve([])
+        return set([tuple(sorted(x)) for x in self.res])
+
+
 
